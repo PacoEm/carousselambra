279c20a4dd4388a243a7f55a2afa902e83008976
Pascal <pascalzic@outlook.fr>
2022-11-24 19:01:50 +0100

    change 11/24/2022

--- navbar.js
+++ navbar.js
@@ -1,7 +1,19 @@
 const navbar = document.getElementById("navbar");
 
-window.addEventListener("load", () => {
-  navbar.style.transformProperty = "translateY";
-  navbar.style.transition = "0.4s ease";
-  navbar.style.transform = "translateY(500px)";
-});
+// window.addEventListener("load", () => {
+//   navbar.style.transformProperty = "translateY";
+//   navbar.style.transition = "0.4s ease";
+//   navbar.style.transform = "translateY(500px)";
+// });
+
+window.onresize = function () {
+  if (window.innerWidth <= 960) {
+    navbar.style.marginLeft = "-100px";
+    navbar.style.transition = "0.4s ease";
+    navbar.style.transitionProperty = "margin-left";
+  } else {
+    navbar.style.marginLeft = "20px";
+    navbar.style.transition = "0.4s ease";
+    navbar.style.transitionProperty = "margin-left";
+  }
+};
